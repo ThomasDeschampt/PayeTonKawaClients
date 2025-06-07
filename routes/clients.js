9934db348b22575568c18d39a3cf1cc21e921371
Beezell <abigael.castillo@live.fr>
2025-06-07 12:46:43 +0200

    modifier

--- routes/clients.js
+++ routes/clients.js
@@ -7,9 +7,11 @@ const authorized = require("../middleware/auth");
 const ajouter = require("../controllers/clients/ajouter");
 const afficher = require("../controllers/clients/afficher.js");
 const afficherAll = require("../controllers/clients/afficherAll");
+const modifier = require("../controllers/clients/modifier");
 
 router.post("/ajouter", authorized, ajouter);
 router.get("/afficher/:uuid", authorized, afficher);
 router.get("/afficherAll", authorized, afficherAll);
+router.put("/modifier/:uuid", authorized, modifier);
 
 module.exports = router;
