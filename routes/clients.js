911f2c2b9da62bf89aef3f9082df89afe854ea85
Beezell <abigael.castillo@live.fr>
2025-06-07 12:42:31 +0200

    afficher

--- routes/clients.js
+++ routes/clients.js
@@ -5,9 +5,11 @@ const authorized = require("../middleware/auth");
 
 //controllers de clients
 const ajouter = require("../controllers/clients/ajouter");
+router.get("/afficher/:uuid", authorized, afficher);
 const afficherAll = require("../controllers/clients/afficherAll");
 
 router.post("/ajouter", authorized, ajouter);
+router.get("/afficher/:uuid", authorized, afficher);
 router.get("/afficherAll", authorized, afficherAll);
 
 module.exports = router;
