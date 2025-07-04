125ba5ff3a962764eac2b6d64d7f7bdb84f4112b
thomas.deschampt@epsi.fr <thomas.deschampt@epsi.fr>
2025-07-04 15:54:36 +0200

    passage du cryptage aes vers gcm

--- middleware/crypt.js
+++ middleware/crypt.js
@@ -1,6 +1,34 @@
 const crypto = require('crypto');
 const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY;
 const IV_LENGTH = 16;
+const IV_LENGTH_GCM = 12;
+
+function encrypt_v2(text) {
+  const iv = crypto.randomBytes(IV_LENGTH);
+  const cipher = crypto.createCipheriv('aes-256-gcm', Buffer.from(ENCRYPTION_KEY), iv);
+
+  let encrypted = cipher.update(text, 'utf8', 'hex');
+  encrypted += cipher.final('hex');
+
+  const authTag = cipher.getAuthTag();
+
+  return iv.toString('hex') + ':' + authTag.toString('hex') + ':' + encrypted;
+}
+
+function decrypt_v2(text) {
+  const [ivHex, authTagHex, encrypted] = data.split(':');
+
+  const iv = Buffer.from(ivHex, 'hex');
+  const authTag = Buffer.from(authTagHex, 'hex');
+
+  const decipher = crypto.createDecipheriv('aes-256-gcm', ENCRYPTION_KEY, iv);
+  decipher.setAuthTag(authTag);
+
+  let decrypted = decipher.update(encrypted, 'hex', 'utf8');
+  decrypted += decipher.final('utf8');
+
+  return decrypted;
+}
 
 function encrypt(text) {
   const iv = crypto.randomBytes(IV_LENGTH);
@@ -22,4 +50,6 @@ function decrypt(text) {
 module.exports = {
   encrypt,
   decrypt,
+  encrypt_v2,
+  decrypt_v2
 }
